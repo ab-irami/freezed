bdfe7189d799b91ab2c8c8eb22581344c132b909
ab-irami <abiramisd16@gmail.com>
2024-02-13 21:54:08 +0530

    documentation

--- lib/model/example_model.dart
+++ lib/model/example_model.dart
@@ -1,7 +1,12 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+/// FIRST YOU NEED TO CREATE A CLASS WITH @freezed annotation AND MIXIN & REQUIRED CONSTRUCTOR
+///ADD THIS TWO LINE BEFORE BUILD RUNNER COMMAND
 part 'example_model.freezed.dart';
 part 'example_model.g.dart';
+
+/// flutter pub run build_runner watch --delete-conflicting-outputs
+/// RUN THIS COMMAND TO GENERATE CODE FOR MODELS
 
 @freezed
 @JsonSerializable(genericArgumentFactories: true)
